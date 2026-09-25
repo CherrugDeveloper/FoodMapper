7738d023b7e29848c2a8acbd0f8b70e7b5b11b14
CherrugDeveloper <marco.falco1997@gmail.com>
2026-09-25 12:45:38 +0200

    fixes App import to use extensionless path

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App'
 // AGGIORNATO: Carica la configurazione multilingua all'avvio dell'app
 import './i18n'
 
